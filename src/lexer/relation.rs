f9f3b456c15eff17a14ce8a298a4c24ebc31c372
PzzaZZa <arick.kfn@gmail.com>
2025-07-13 12:23:48 +0300

    RelationTrait?

--- src/lexer/relation.rs
+++ src/lexer/relation.rs
@@ -2,4 +2,8 @@ use crate::prelude::*;
 pub struct Relation {
     stucture: Vec<RelationComponent>,
     name: String,
+    ordered: bool,
+}
+pub trait RelationTrait {
+    fn structure() -> 
 }
--- src/lexer/relation_component.rs
+++ src/lexer/relation_component.rs
@@ -3,4 +3,5 @@ use crate::prelude::*;
 pub struct RelationComponent {
     token_chain: Vec<Token>,
     name: String,
+    ordered: bool,
 }
